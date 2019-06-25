0886836668edf9de9c2341943c47c14c7fb62031
analyst <analyst@elk.windomain.local>
2019-06-24 21:32:26 -0500

    Add wait for input to run-elk.sh

--- run-elk.sh
+++ run-elk.sh
@@ -5,3 +5,5 @@ ansible-playbook /opt/playbooks/elk-docker/update-run-elk-containers.yml
 cd /opt/playbooks/windows-dc
 
 /opt/playbooks/windows-dc/config-dns.sh
+
+read -p "Press key to continue... " -n1 -s
